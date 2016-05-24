88887ecc642a600cabbb6d70c93c7b8bccfc5977
Chris Undernehr <chris@walmartlabs.com>
2016-05-24 00:31:06 -0500

    DTBFDTECH-227: Update node.properties file for presto

--- components/cookbooks/presto/recipes/add.rb
+++ components/cookbooks/presto/recipes/add.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 include_recipe "presto::delete"
 
 package 'mosh'
@@ -10,6 +12,33 @@ template '/opt/nagios/libexec/check_presto.rb' do
     owner 'oneops'
     group 'oneops'
     mode '0755'
+end
+
+
+directory node.presto.data_directory_dir do
+  owner 'presto'
+  group 'presto'
+  mode  '0755'
+  recursive true
+end
+
+directory '/etc/presto' do
+  owner 'presto'
+  group 'presto'
+  mode  '0755'
+  recursive true
+end
+
+template '/etc/presto/node.properties' do
+    source 'node.properties.erb'
+    owner 'presto'
+    group 'presto'
+    mode '0755'
+    variables ({
+        :environment => node.workorder.payLoad.Environment[0].ciName,
+        :node_id => SecureRandom.uuid,
+        :data_directory_dir => node.presto.data_directory_dir
+    })
 end
 
 include_recipe "presto::start"
