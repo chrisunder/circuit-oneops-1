115036b54d3492ae6d11225f6f8af6bf5bef19e7
Chris Undernehr <chris@walmartlabs.com>
2016-05-24 00:30:09 -0500

    DTBFDTECH-227: Remove unneeded sub components

--- packs/presto.rb
+++ packs/presto.rb
@@ -5,7 +5,6 @@ description 'presto'
 type 'Platform'
 category 'Other'
 
-environment 'redundant', {}
 resource 'compute',
          :cookbook => 'oneops.1.compute',
          :design => true,
@@ -35,25 +34,6 @@ resource 'presto',
                                   'value' => metric(:unit => '', :description => 'value', :dstype => 'DERIVE')
 
                               } },
-             'Log' => { :description => 'Log',
-                        :source => '',
-                        :chart => { 'min' => 0, 'unit' => '' },
-                        :cmd => 'check_logfiles!logpresto!#{cmd_options[:logfile]}!#{cmd_options[:warningpattern]}!#{cmd_options[:criticalpattern]}',
-                        :cmd_line => '/opt/nagios/libexec/check_logfiles   --noprotocol --tag=$ARG1$ --logfile=$ARG2$ --warningpattern="$ARG3$" --criticalpattern="$ARG4$"',
-                        :cmd_options => {
-                            'logfile' => '!#{cmd_options[:logfile]}',
-                            'warningpattern' => 'WARNING',
-                            'criticalpattern' => 'CRITICAL'
-                        },
-                        :metrics => {
-                            'logpresto_lines' => metric(:unit => 'lines', :description => 'Scanned Lines', :dstype => 'GAUGE'),
-                            'logpresto_warnings' => metric(:unit => 'warnings', :description => 'Warnings', :dstype => 'GAUGE'),
-                            'logpresto_criticals' => metric(:unit => 'criticals', :description => 'Criticals', :dstype => 'GAUGE'),
-                            'logpresto_unknowns' => metric(:unit => 'unknowns', :description => 'Unknowns', :dstype => 'GAUGE')
-                        },
-                        :thresholds => {
-                            'CriticalLogException' => threshold('15m', 'avg', 'logpresto_criticals', trigger('>=', 1, 15, 1), reset('<', 1, 15, 1))
-                        } },
              'JvmInfo' => { :description => 'JvmInfo',
                             :source => '',
                             :chart => { 'min' => 0, 'unit' => '' },
@@ -68,32 +48,6 @@ resource 'presto',
                             :thresholds => {
                                 'HighMemUse' => threshold('5m', 'avg', 'percentUsed', trigger('>', 98, 15, 1), reset('<', 98, 5, 1))
                             } }
-         }
-
-resource 'presto-daemon',
-         :cookbook => 'oneops.1.daemon',
-         :design => true,
-         :requires => {
-             :constraint => '0..1',
-             :help => 'Restarts Presto'
-         },
-         :attributes => {
-             :service_name => 'presto',
-             :use_script_status => 'true',
-             :pattern => ''
-         },
-         :monitors => {
-             'prestoprocess' => { :description => 'PrestoProcess',
-                                  :source => '',
-                                  :chart => { 'min' => '0', 'max' => '100', 'unit' => 'Percent' },
-                                  :cmd => 'check_process!:::node.workorder.rfcCi.ciAttributes.service_name:::!:::node.workorder.rfcCi.ciAttributes.use_script_status:::!:::node.workorder.rfcCi.ciAttributes.pattern:::!:::node.workorder.rfcCi.ciAttributes.secondary_down:::',
-                                  :cmd_line => '/opt/nagios/libexec/check_process.sh "$ARG1$" "$ARG2$" "$ARG3$" "$ARG4$"',
-                                  :metrics => {
-                                      'up' => metric(:unit => '%', :description => 'Percent Up')
-                                  },
-                                  :thresholds => {
-                                      'PrestoDaemonProcessDown' => threshold('1m', 'avg', 'up', trigger('<=', 98, 1, 1), reset('>', 95, 1, 1))
-                                  } }
          }
 
 resource 'artifact',
@@ -124,26 +78,7 @@ resource 'artifact',
                         },
                         :thresholds => {
 
-                        } },
-             'exceptions' => { :description => 'Exceptions',
-                               :source => '',
-                               :chart => { 'min' => 0, 'unit' => '' },
-                               :cmd => 'check_logfiles!logexc!#{cmd_options[:logfile]}!#{cmd_options[:warningpattern]}!#{cmd_options[:criticalpattern]}',
-                               :cmd_line => '/opt/nagios/libexec/check_logfiles   --noprotocol  --tag=$ARG1$ --logfile=$ARG2$ --warningpattern="$ARG3$" --criticalpattern="$ARG4$"',
-                               :cmd_options => {
-                                   'logfile' => '/log/logmon/logmon.log',
-                                   'warningpattern' => 'Exception',
-                                   'criticalpattern' => 'Exception'
-                               },
-                               :metrics => {
-                                   'logexc_lines' => metric(:unit => 'lines', :description => 'Scanned Lines', :dstype => 'GAUGE'),
-                                   'logexc_warnings' => metric(:unit => 'warnings', :description => 'Warnings', :dstype => 'GAUGE'),
-                                   'logexc_criticals' => metric(:unit => 'criticals', :description => 'Criticals', :dstype => 'GAUGE'),
-                                   'logexc_unknowns' => metric(:unit => 'unknowns', :description => 'Unknowns', :dstype => 'GAUGE')
-                               },
-                               :thresholds => {
-                                   'CriticalExceptions' => threshold('15m', 'avg', 'logexc_criticals', trigger('>=', 1, 15, 1), reset('<', 1, 15, 1))
-                               } }
+                        } }
          }
 
 resource 'build',
@@ -190,10 +125,8 @@ resource 'java',
          }
 
 # depends_on
-[{ :from => 'presto', :to => 'os' },
- { :from => 'presto', :to => 'user' },
- { :from => 'presto-daemon', :to => 'compute' },
- { :from => 'presto',     :to => 'java' },
+[{ :from => 'presto', :to => 'user' },
+ { :from => 'presto', :to => 'java' },
  { :from => 'artifact',   :to => 'library' },
  { :from => 'artifact',   :to => 'presto'  },
  { :from => 'artifact',   :to => 'download' },
@@ -202,8 +135,6 @@ resource 'java',
  { :from => 'build',      :to => 'library' },
  { :from => 'build',      :to => 'presto'  },
  { :from => 'build',      :to => 'download' },
- { :from => 'daemon',     :to => 'artifact' },
- { :from => 'daemon',     :to => 'build' },
  { :from => 'java',       :to => 'compute' },
  { :from => 'java',       :to => 'os' },
  { :from => 'java',       :to => 'download' }].each do |link|
@@ -214,14 +145,8 @@ resource 'java',
              :attributes => { 'flex' => false, 'min' => 1, 'max' => 1 }
 end
 
-relation 'presto-daemon::depends_on::artifact',
-         :relation_name => 'DependsOn',
-         :from_resource => 'presto-daemon',
-         :to_resource => 'artifact',
-         :attributes => { 'propagate_to' => 'from', 'flex' => false, 'min' => 1, 'max' => 1 }
-
 # managed_via
-['presto', 'artifact', 'build', 'java', 'presto-daemon'].each do |from|
+['presto', 'build', 'artifact', 'java', ].each do |from|
     relation "#{from}::managed_via::compute",
              :except => ['_default'],
              :relation_name => 'ManagedVia',
