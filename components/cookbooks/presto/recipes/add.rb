de02cbefb5657bd67d1875cc1b99a184451903ab
Chris Undernehr <chris@walmartlabs.com>
2016-05-31 09:18:35 -0500

    DTBFDTECH-227: Refactor presto install

--- components/cookbooks/presto/recipes/add.rb
+++ components/cookbooks/presto/recipes/add.rb
@@ -12,46 +12,6 @@ include_recipe "presto::delete"
 package 'mosh'
 package 'screen'
 
-include_recipe "presto::install_binary"
-
-template '/opt/nagios/libexec/check_presto.rb' do
-    source 'check_presto.rb.erb'
-    owner 'oneops'
-    group 'oneops'
-    mode '0755'
-end
-
-directory node.presto.data_directory_dir do
-  owner 'presto'
-  group 'presto'
-  mode  '0755'
-  recursive true
-end
-
-directory '/usr/lib/presto/var' do
-  owner 'presto'
-  group 'presto'
-  mode  '0755'
-  recursive true
-end
-
-directory '/etc/presto' do
-  owner 'presto'
-  group 'presto'
-  mode  '0755'
-  recursive true
-end
-
-template '/etc/presto/node.properties' do
-    source 'node.properties.erb'
-    owner 'presto'
-    group 'presto'
-    mode '0755'
-    variables ({
-        :environment => node.workorder.payLoad.Environment[0].ciName,
-        :node_id => SecureRandom.uuid,
-        :data_directory_dir => node.presto.data_directory_dir
-    })
-end
+include_recipe "presto::install"
 
 include_recipe "presto::start"
---
+++ components/cookbooks/presto/recipes/install.rb
@@ -0,0 +1,46 @@
+ruby_block 'Install Presto RPM' do
+    block do
+        Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
+        system("yum install -y 'http://cdc-rgw.ost.cloud.wal-mart.com/swift/v1/com-walmart-bfd-public-installs/presto/presto-server-rpm-#{node.presto.version}.rpm'")
+    end
+end
+
+template '/opt/nagios/libexec/check_presto.rb' do
+    source 'check_presto.rb.erb'
+    owner 'oneops'
+    group 'oneops'
+    mode '0755'
+end
+
+directory node.presto.data_directory_dir do
+  owner 'presto'
+  group 'presto'
+  mode  '0755'
+  recursive true
+end
+
+directory '/usr/lib/presto/var' do
+  owner 'presto'
+  group 'presto'
+  mode  '0755'
+  recursive true
+end
+
+directory '/etc/presto' do
+  owner 'presto'
+  group 'presto'
+  mode  '0755'
+  recursive true
+end
+
+template '/etc/presto/node.properties' do
+    source 'node.properties.erb'
+    owner 'presto'
+    group 'presto'
+    mode '0755'
+    variables ({
+        :environment => node.workorder.payLoad.Environment[0].ciName,
+        :node_id => SecureRandom.uuid,
+        :data_directory_dir => node.presto.data_directory_dir
+    })
+end
